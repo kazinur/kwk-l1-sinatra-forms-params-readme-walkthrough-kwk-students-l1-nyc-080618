d3c54bc3f4e7d53a85d0f402c933d04fe6624e90
kazinur <kazinur@mybihs.org>
2018-08-16 15:17:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,60 @@
+# require_relative 'config/environment'
+# #this is route. This is taking us to food_form. 
+# class App < Sinatra::Base
+
+#   get '/food_form' do
+#     #user have to type it. 
+#     erb :food_form
+#   end
+
+#   # Add your post route and action below
+
+# post '/food' do 
+#   #post because it will take to a new page
+#   erb :food 
+
+
+
+# end 
+
+
+# end
+
+
+
+# require_relative 'config/environment'
+# #this is route. This is taking us to food_form. 
+# class App < Sinatra::Base
+
+#   get '/pizza_form' do
+#     #user have to type it. 
+#     erb :pizza_form
+#   end
+
+#   # Add your post route and action below
+
+# post '/pizza' do 
+#   #post because it will take to a new page
+#   erb :pizza 
+
+
+
+
 require_relative 'config/environment'
 #this is route. This is taking us to food_form. 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
-  end
+  get '/suitKASE_form' do
+    #user have to type it. 
+    erb :subscribe 
+  end 
 
   # Add your post route and action below
 
-post '/food' do 
-"My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+# get '/suitKASE' do 
+#   #post because it will take to a new page
+#   erb :subscribe
 
-
-
-end 
 
 
 end
